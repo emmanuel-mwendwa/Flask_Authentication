8b324f3b5a4ea050e6656d4991fd6dec8359af33
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-10-19 19:37:44 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    APP_ADMIN = os.environ.get("APP_ADMIN")
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data-dev.sqlite")
+
+
+class TestingConfig(Config):
+    pass
+
+
+class ProductionConfig(Config):
+    pass
+
+
+config = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+
+    "default": DevelopmentConfig
+}
